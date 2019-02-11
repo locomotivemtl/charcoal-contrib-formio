659baa5bb1942b6f435cab1393f2ce2647fb3834
Joel Alphonso <joel@locomotive.ca>
2019-02-11 11:22:29 -0500

    Compiled assets

--- assets/dist/scripts/formio.js
+++ assets/dist/scripts/formio.js
@@ -9,7 +9,7 @@
 
     /***
      * `charcoal/admin/property/input/Formio/Form`
-     * Property_Input_FormBuilder_Widget Javascript class
+     * Property_Input_Formio_Form Javascript class
      *
      */
     var FormBuilder = function (data) {
@@ -70,6 +70,10 @@
             {builder: options}
         )
             .then(function (builder) {
+                if (options.hasOwnProperty('submission')) {
+                    builder.submission = options.submission;
+                }
+
                 // On builder render
                 that.$loader.removeClass('-is-loading');
 
@@ -91,5 +95,201 @@
     };
 
     Charcoal.Admin.Property_Input_Formio_Form = FormBuilder;
+
+}(jQuery, document, Formio));
+;/* global Formio, Charcoal */
+/**
+ * Form Schema builder
+ *
+ * Require
+ * - formio.full.min.js
+ */
+;(function () {
+
+    /***
+     * `charcoal/admin/property/input/Formio/Form`
+     * Property_Input_Formio_Schema Javascript class
+     *
+     */
+    var FormBuilder = function (data) {
+        Charcoal.Admin.Property.call(this, data);
+
+        this.set_properties(data).init();
+    };
+
+    FormBuilder.prototype             = Object.create(Charcoal.Admin.Property.prototype);
+    FormBuilder.prototype.constructor = Charcoal.Admin.Property_Input_Formio_Schema;
+    FormBuilder.prototype.parent      = Charcoal.Admin.Property.prototype;
+
+    FormBuilder.prototype.set_properties = function (data) {
+        // Builder reference
+        this._builder         = undefined;
+        this._builder_options = data.data.builder_options;
+
+        // Elements
+        this.$widget  = this.element();
+        this.$builder = this.$widget.find('.js-form-builder');
+        this.$input   = this.$widget.find('.js-input');
+        this.$loader  = this.$widget.find('.js-loader');
+
+        return this;
+    };
+
+    FormBuilder.prototype.init = function () {
+        var that = this;
+
+        this.$loader.addClass('-is-loading');
+        if (typeof Formio === "undefined") {
+            $.getScript('https://cdn.jsdelivr.net/npm/formiojs@3.13.10/dist/formio.full.min.js').done(function () {
+                that.init();
+            });
+
+            return;
+        }
+
+        var options = this.parse_options();
+        this.init_builder(options);
+
+    };
+
+    FormBuilder.prototype.parse_options = function () {
+        var defaultOptions = {};
+
+        this._builder_options = $.extend(true, defaultOptions, this._builder_options);
+
+        return this._builder_options;
+    };
+
+    FormBuilder.prototype.init_builder = function (options) {
+        var that = this;
+        var initialData = this.$input.val() ? JSON.parse(this.$input.val()) : {};
+
+        Formio.builder(
+            this.$builder.get(0),
+            initialData,
+            {builder: options}
+        )
+            .then(function (builder) {
+                // On builder render
+                that.$loader.removeClass('-is-loading');
+
+                that._builder = builder;
+            });
+    };
+
+    /**
+     * I believe this should fit the PHP model
+     * Added the save() function to be called on form submit
+     * Could be inherited from a global Charcoal.Admin.Property Prototype
+     * Extra ideas:
+     * - save
+     * - validate
+     * @return this (chainable)
+     */
+    FormBuilder.prototype.save = function () {
+        this.$input.val(JSON.stringify(this._builder.schema));
+    };
+
+    Charcoal.Admin.Property_Input_Formio_Schema = FormBuilder;
+
+}(jQuery, document));
+;/* global Formio, Charcoal */
+/**
+ * Form builder
+ *
+ * Require
+ * - formio.full.min.js
+ */
+;(function () {
+
+    /**
+     * `charcoal/admin/property/input/Formio/Form`
+     * Property_Input_Formio_Form Javascript class
+     *
+     */
+    var FormBuilder = function (data) {
+        Charcoal.Admin.Property.call(this, data);
+
+        this.set_properties(data).init();
+    };
+
+    FormBuilder.prototype             = Object.create(Charcoal.Admin.Property.prototype);
+    FormBuilder.prototype.constructor = Charcoal.Admin.Property_Input_Formio_Submission;
+    FormBuilder.prototype.parent      = Charcoal.Admin.Property.prototype;
+
+    FormBuilder.prototype.set_properties = function (data) {
+        // Builder reference
+        this._builder         = undefined;
+        this._builder_options = data.data.builder_options;
+        this._schema          = data.data.schema;
+
+        this.save_action   = data.save_action || 'object/save';
+        this.update_action = data.update_action || 'object/update';
+
+        // Elements
+        this.$widget  = this.element();
+        this.$builder = this.$widget.find('.js-form-builder');
+        this.$input   = this.$widget.find('.js-input');
+        this.$loader  = this.$widget.find('.js-loader');
+
+        return this;
+    };
+
+    FormBuilder.prototype.init = function () {
+        this.$loader.addClass('-is-loading');
+        if (typeof Formio === "undefined") {
+            console.log('Missing formio.full.js dependencies');
+
+            return;
+        }
+
+        var options = this.parse_options();
+        this.init_builder(options);
+
+    };
+
+    FormBuilder.prototype.parse_options = function () {
+        var defaultOptions = {};
+
+        this._builder_options = $.extend(true, defaultOptions, this._builder_options);
+
+        return this._builder_options;
+    };
+
+    FormBuilder.prototype.init_builder = function (options) {
+        var that           = this;
+        var submissionData = this.$input.val() ? JSON.parse(this.$input.val()) : {};
+
+        Formio.createForm(
+            this.$builder.get(0),
+            this._schema,
+            options
+        )
+            .then(function (builder) {
+                if (submissionData !== null) {
+                    builder.submission = submissionData;
+                }
+
+                // On builder render
+                that.$loader.removeClass('-is-loading');
+
+                that._builder = builder;
+            });
+    };
+
+    /**
+     * I believe this should fit the PHP model
+     * Added the save() function to be called on form submit
+     * Could be inherited from a global Charcoal.Admin.Property Prototype
+     * Extra ideas:
+     * - save
+     * - validate
+     * @return this (chainable)
+     */
+    FormBuilder.prototype.save = function () {
+        this.$input.val(JSON.stringify(this._builder.schema));
+    };
+
+    Charcoal.Admin.Property_Input_Formio_Submission = FormBuilder;
 
 }(jQuery, document, Formio));
